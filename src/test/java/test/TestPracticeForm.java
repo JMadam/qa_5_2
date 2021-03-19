538bd4ad889abfcf0d83731e74223666568811ca
yuliyamadaminova <Cdtnjxrf2103>
2021-03-19 13:22:36 +0300

    Improvements after CR

--- src/test/java/test/TestPracticeForm.java
+++ src/test/java/test/TestPracticeForm.java
@@ -1,8 +1,6 @@
 package test;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -26,45 +24,53 @@ public class TestPracticeForm {
     void successfulFillTest() {
         String firstName = "Julia";
         String lastName = "Madam";
+        String userEmail = "Madam@example.com";
+        String userNumber = "1234567890";
+        String gender = "Female";
+        String birthDay = "13";
+        String birthMonth = "August";
+        String birthYear = "1985";
+        String birthCheck ="13 August,1985";
+        String subjects = "Biology";
+        String hobby = "Music";
+        String picture = "1.jpg";
+        String address = "SPB";
+        String state = "Haryana";
+        String city = "Karnal";
+
+
 
         open("https://demoqa.com/automation-practice-form");
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
-        $("#userEmail").setValue("Madam@example.com");
-        $("[for='gender-radio-2']").click();
-        $("#userNumber").setValue("1234567890");
+        $("#userEmail").setValue(userEmail);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue("7");
-        $(".react-datepicker__year-select").selectOptionByValue("1985");
-        $("[aria-label='Choose Tuesday, August 13th, 1985']").click();
-        $("#subjectsContainer").click();
-        $("#subjectsInput").setValue("Biology").pressEnter();
-        $("#subjectsInput").setValue("Physics").pressEnter();
-        $(byText("Sports")).click();
-        $(byText("Reading")).click();
-        $(byText("Music")).click();
-        $("#uploadPicture").uploadFromClasspath("1.jpg");
-        $("#currentAddress").setValue("SPB");
-        $("#state").click();
-        $("#react-select-3-input").setValue("Haryana").pressEnter();
-        $("#city").click();
-        $("#react-select-4-input").setValue("Karnal").pressEnter();
+        $(".react-datepicker__month-select").selectOption(birthMonth);
+        $(".react-datepicker__year-select").selectOption(birthYear);
+        $(String.format("[aria-label='Choose Tuesday, %s %sth, %s']", birthMonth, birthDay, birthYear)).click();
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $("[for='hobbies-checkbox-3']").click();
+        $("#uploadPicture").uploadFile(new File("./src/test/java/../resources/"+ picture));
+        $("#currentAddress").setValue(address);
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
         $("#submit").click();
 
 
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
         $(byText(firstName + " " + lastName)).shouldBe(visible);
-        $(".modal-content").shouldHave(text("Madam@example.com"),
-                text("Female"),
-                text("1234567890"),
-                text("13 August,1985"),
-                text("Biology, Physics"),
-                text("Sports, Reading, Music"),
-                text("1.jpg"),
-                text("SPB"),
-                text("Haryana"),
-                text("Karnal"));
-        $("#closeLargeModal").click();
+        $(".modal-body").$(byText(gender)).shouldHave(text(gender));
+        $(byText(userNumber)).shouldBe(visible);
+        $(byText(userEmail)).shouldBe(visible);
+        $(byText(birthCheck)).shouldBe(visible);
+        $(byText(subjects)).shouldBe(visible);
+        $(byText(hobby)).shouldBe(visible);
+        $(byText(picture)).shouldBe(visible);
+        $(byText(address)).shouldBe(visible);
+        $(byText(state+" "+city)).shouldBe(visible);
+
     }
 
 
